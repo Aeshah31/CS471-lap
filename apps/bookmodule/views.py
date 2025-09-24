592b125a1c2bd0f6d4ff0709da8565ce44adb6b6
Aeshah31 <ayooshy55@gmail.com>
2025-09-25 02:15:09 +0300

    Task 5 - lap3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,11 +1,16 @@
 
 from django.shortcuts import render 
+
 def index(request):
     name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html")
+    return render(request, "bookmodule/index.html" , {"name": name})  
+
 
 
  
+def index2(request, val1 = 0):  
+    return render("value1 = "+str(val1))
+
 
 
 
