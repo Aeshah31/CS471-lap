d3e7489d12c6cbf6cd2f60fa58a3f49f5099d97b
Aeshah31 <ayooshy55@gmail.com>
2025-09-25 01:36:40 +0300

    Task 3 - Lap3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 def index(request):
-    name = request.GET.get("name") or "world!"  #add this line
+    name = request.GET.get("name") or "world!"  
     return HttpResponse("Hello, "+name)
 
+ 
+def index2(request, val1 = 0):  
+    return HttpResponse("value1 = "+str(val1))
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -20,10 +20,10 @@ from django.contrib import admin
 from django.urls import path, include
 import apps.bookmodule.views
 
+ 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', apps.bookmodule.views.index)  
-
-    
+    path('admin/', admin.site.urls),
+    path('', apps.bookmodule.views.index),  
+    path('index2/<int:val1>/', apps.bookmodule.views.index2) 
 ]
 
