3db0057a003de73d60faa97bb3c3dc7b594fdde7
Aeshah31 <ayooshy55@gmail.com>
2025-09-25 01:05:50 +0300

    Task1 Lap3

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from .models import Book
-
-def book_list(request):
-    books = Book.objects.order_by("-created_at")
-    return render(request, "bookmodule/book_list.html", {"books": books})
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello, world!")
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -18,9 +18,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+import apps.bookmodule.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("books/", include("apps.bookmodule.urls")),
+    path('', apps.bookmodule.views.index)  
+
+    
 ]
 
