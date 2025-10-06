eba5271af9b69e5c6bcd54d6c3c17e9a4a0e73d5
Aeshah31 <ayooshy55@gmail.com>
2025-10-06 14:06:46 +0300

    Lap 4

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include("apps.bookmodule.urls")), #include urls.py of bookmodule app
     path('users/', include("apps.usermodule.urls"))  #include urls.py of usermodule app
+
 ]
